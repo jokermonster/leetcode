3d50dc91013b89ff5ac1b5e2eb3cbc8e03fbfdd5
jokermon <1009806353@qq.com>
2020-12-22 12:29:33 +0800

    一道递归调用的二叉树类型题目

---
+++ leetcode/src/jianzhioffer_27.java
@@ -0,0 +1,45 @@
+public class jianzhioffer_27 {
+    /*
+    剑指 Offer 27. 二叉树的镜像
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+
+
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+     */
+    public TreeNode mirrorTree(TreeNode root) {//循环交换
+
+        if(root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(tmp);
+        return root;
+
+    }
+
+}
