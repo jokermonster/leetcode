c52fd3e4eacd466544a20f43b2fdba1f24f5818f
jokermon <1009806353@qq.com>
2020-12-26 10:28:12 +0800

    单调栈的使用，未做出来。

--- leetcode/src/leetcode_1476.java
+++ leetcode/src/leetcode_1476.java
@@ -1,6 +1,6 @@
 public class leetcode_1476 {
     /*
-请你实现一个类 SubrectangleQueries ，它的构造函数的参数是一个 rows x cols 的矩形（这里用整数矩阵表示），并支持以下两种操作：
+请你实现一个类SubrectangleQueries ，它的构造函数的参数是一个 rows x cols 的矩形（这里用整数矩阵表示），并支持以下两种操作：
 1. updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
 用 newValue 更新以 (row1,col1) 为左上角且以 (row2,col2) 为右下角的子矩形。
 2. getValue(int row, int col)
