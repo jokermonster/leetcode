c288e07c9ee6832d3bd6365f4d52963096bdbf9f
jokermon <1009806353@qq.com>
2021-01-05 19:17:59 +0800

    两数之和

---
+++ leetcode/src/leetcode_2.java
@@ -0,0 +1,43 @@
+public class leetcode_2 {
+    /*
+    给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+
+示例 1：
+
+
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+示例 2：
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+示例 3：
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        int carry = 0, newVal = 0;
+        while (p1 != null || p2 != null || carry > 0) {
+            newVal = (p1 == null ? 0: p1.val) + (p2 == null ? 0: p2.val) + carry;
+            carry = newVal / 10;
+            newVal %= 10;
+            p.next = new ListNode(newVal);
+            p1 = p1 == null? null: p1.next;
+            p2 = p2 == null? null: p2.next;
+            p = p.next;
+        }
+        return dummy.next;
+
+    }
+}
