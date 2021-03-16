a7fd07ce0dac0bb0f0be9a9ae51c4f193f01c96f
jokermon <1009806353@qq.com>
2021-03-16 18:32:00 +0800

    螺旋矩阵

---
+++ leetcode/src/union_model.java
@@ -0,0 +1,13 @@
+public class union_model {
+    //并查集模板
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+}
