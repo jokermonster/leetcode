e3fe5da2a64f3b2af20770b3c1682e6caa6cc1f2
jokermon <1009806353@qq.com>
2021-03-25 09:19:48 +0800

    删除重复元素

---
+++ leetcode/src/leetcode_82.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class leetcode_82 {
+    /*
+    82. 删除排序链表中的重复元素 II
+给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+
+示例 1:
+
+输入: 1->2->3->3->4->4->5
+输出: 1->2->5
+示例 2:
+
+输入: 1->1->1->2->3
+输出: 2->3
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+
+
+    }
+}
